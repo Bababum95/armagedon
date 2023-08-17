be098a7cc96aede02fa94cbd2e7834f2a8f96873
bababum <bababum1995@gmail.com>
2023-08-17 14:14:59 +0700

    check-point

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { IAsteroidData } from '@/interfaces/IAsteroidData';
-import PageClientContent from '@/components/AsteroidsList/PageClientContent';
+
+import styles from './page.module.css'
+import Asteroid from '@/components/Asteroid/Asteroid';
 
 const getToday = () => {
   const today = new Date();
@@ -22,8 +24,20 @@ export default async function Home() {
   const asteroidData = await getAsteroids()
 
   return (
-    <>
-      <PageClientContent asteroidData={asteroidData} />
-    </>
+    <div>
+    <h1 className={styles.title}>Ближайшие подлёты астероидов</h1>
+    <ul className={styles.list}>
+        {asteroidData.map((asteroid) => (
+            <Asteroid
+                key={asteroid.id}
+                name={asteroid.name}
+                diameter={asteroid.estimated_diameter.kilometers.estimated_diameter_max}
+                dangerous={asteroid.is_potentially_hazardous_asteroid}
+                date={asteroid.close_approach_data[0].close_approach_date_full}
+                distance={asteroid.close_approach_data[0].miss_distance.kilometers}
+            />
+        ))}
+    </ul>
+</div>
   )
 }
--- components/Asteroid/Asteroid.tsx
+++ components/Asteroid/Asteroid.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { FC } from 'react'
 import styles from './Asteroid.module.css'
 import Image from 'next/image';
