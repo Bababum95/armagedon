804550f5879809c74890b918241de5338014ad63
bababum <bababum1995@gmail.com>
2023-08-24 14:29:07 +0700

    fix data for safari

--- utils/formattedDate.ts
+++ utils/formattedDate.ts
@@ -1,7 +1,27 @@
-import { format } from 'date-fns';
-import { ru } from 'date-fns/locale';
+import moment from "moment";
+
+const monthMap: { [key: string]: string } = {
+    Jan: 'Янв',
+    Feb: 'Фев',
+    Mar: 'Мар',
+    Apr: 'Апр',
+    May: 'Май',
+    Jun: 'Июн',
+    Jul: 'Июл',
+    Aug: 'Авг',
+    Sep: 'Сен',
+    Oct: 'Окт',
+    Nov: 'Ноя',
+    Dec: 'Дек',
+  };
+  
 
 export const formattedDate = (date: string) => {
-    const parsedDate = new Date(date);
-    return format(parsedDate, 'dd MMM yyyy', { locale: ru }).replace(/\./g, '');
+    const parsedDate = moment(date, 'YYYY-MMM-DD');
+    const russianMonth = Object.keys(monthMap).reduce(
+        (formattedMonth, englishMonth) =>
+            formattedMonth.replace(englishMonth, monthMap[englishMonth]),
+        parsedDate.format('MMM')
+    );
+    return parsedDate.format(`DD ${russianMonth} YYYY`);
 };
